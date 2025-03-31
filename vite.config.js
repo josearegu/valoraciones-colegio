1fe4decaf11e55e17eebd0b2085ef7dc56c713ce
Jose_Luis_Arevalo <josearegu@gmail.com>
2025-03-30 19:25:11 -0500

    Ajustar base URL para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/valoraciones-colegio/',
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
