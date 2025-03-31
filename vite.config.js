cb6c9055a8c8d8b267a88a626cdd1e58da8dacea
Jose_Luis_Arevalo <josearegu@gmail.com>
2025-03-30 22:01:32 -0500

    Reconstruir con rutas relativas corregidas

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/valoraciones-colegio/',
+  base: '/', // Cambia a ruta relativa
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  build: {
+    chunkSizeWarningLimit: 1500,
   },
 });
