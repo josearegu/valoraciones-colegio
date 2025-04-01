56c937ea42f37b1cc9eaffb332e4e37e1177c3b6
Jose_Luis_Arevalo <josearegu@gmail.com>
2025-04-01 07:59:49 -0500

    Actualizar build con base correcto para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-// vite.config.js
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // Cambia a ruta relativa
+  base: '/valoraciones-colegio/', // Ajusta al nombre de tu repositorio
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
