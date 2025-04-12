5499c05cff6bd1c25946de6762207052117342d1
Jose_Luis_Arevalo <josearegu@gmail.com>
2025-04-12 10:23:49 -0500

    Corrige index.html y ajusta vite.config.js para despliegue

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  base: '/valoraciones-colegio/', // Ajusta al nombre de tu repositorio
-  plugins: [react()],
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
-  },
-  build: {
-    chunkSizeWarningLimit: 1500,
-  },
-  server: {
-    port: 5173,
-  },
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'production' ? '/valoraciones-colegio/' : '/',
+    plugins: [react()],
+    resolve: {
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+    build: {
+      chunkSizeWarningLimit: 1500,
+      // Asegura que los assets se generen en el directorio correcto
+      assetsDir: 'assets',
+      outDir: 'dist',
+      rollupOptions: {
+        output: {
+          // Asegura que los nombres de los archivos generados sean consistentes
+          entryFileNames: 'assets/[name]-[hash].js',
+          chunkFileNames: 'assets/[name]-[hash].js',
+          assetFileNames: 'assets/[name]-[hash][extname]',
+        },
+      },
+    },
+    server: {
+      port: 5173,
+    },
+  };
 });
